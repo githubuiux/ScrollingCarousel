e2e3ac5313ff385b619af60ed0c1bafa7123e561
githubuiux <uiuxista@gmail.com>
2023-06-13 12:03:33 +0300

    Initial Commit

---
+++ ScrollingCarousel/ScrollingCarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollingCarouselApp.swift
+//  ScrollingCarousel
+//
+//  Created by Levent on 13.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrollingCarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
